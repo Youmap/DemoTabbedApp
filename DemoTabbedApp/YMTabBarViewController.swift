cc5e9bf53954027f7cd75a9af0aaf199eb03e763
Michael Valentiner <michael.valentiner@gmail.com>
2018-05-14 16:49:31 -0400

    Added TabBar tab animations.

--- DemoTabbedApp/YMTabBarViewController.swift
+++ DemoTabbedApp/YMTabBarViewController.swift
@@ -56,15 +56,85 @@ class YMTabBarViewController : UIViewController, YMTabBarTabEventDelegate {
 	}
 
 	private func animate(from fromTab: YMTabBarTab, to toTab : YMTabBarTab) {
+		guard fromTab != toTab else {
+			return
+		}
+	
 		// Update the tabbed view controller's content
-		fromTab.uiViewController.view.removeFromSuperview()
-		fromTab.uiViewController.removeFromParentViewController()
-
-		displayView.insertSubview(toTab.uiViewController.view, belowSubview: tabBarView.superview!)
-		parent?.addChildViewController(toTab.uiViewController)
+		animate(from: fromTab.uiViewController, to: toTab.uiViewController)
 
 		// Update the tabs
+		animate(tabView: toTab)
 		fromTab.isSelected = false
 		toTab.isSelected = true
 	}
+
+    private func animate(from fromVC: UIViewController, to toVC: UIViewController) {
+
+		displayView.insertSubview(toVC.view, belowSubview: tabBarView.superview!)
+		parent?.addChildViewController(toVC)
+
+        // Send the current navigation controller view to the back
+        UIView.animate(withDuration: 0.325, animations: {
+            fromVC.view.layer.transform = CATransform3DMakeScale(0.825, 0.825, 1)
+        },
+		completion: { (success: Bool) -> Void in
+			fromVC.view.removeFromSuperview()
+			fromVC.removeFromParentViewController()
+		})
+
+        // Bring the destination navigation controller view to the front
+        let startingY = view.frame.height
+        toVC.view.layer.transform = CATransform3DMakeTranslation(0, startingY, 0)
+        let strongOut = RALUnitBezier.bezier(withControlPoints: 0.385, 1.180, 0.155, 0.990)!
+		
+        let _ = RALAnimation.start(withDuration: 0.5, delay: 0, ease: EaseLinear, tick: { value in
+            let strongOutValue = CGFloat(strongOut(value))
+            let transform = CATransform3DMakeTranslation(0, startingY - (startingY * strongOutValue), 0)
+            toVC.view.layer.transform = transform
+			
+        },
+        onComplete: { completed in
+            toVC.view.layer.transform = CATransform3DIdentity
+        })
+	}
+
+    private func animate(tabView : UIView) {
+        UIView.animate(withDuration: 0.15, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.5, options: .layoutSubviews,
+            animations: {
+            	tabView.transform = CGAffineTransform.identity.scaledBy(x: 1.2, y: 1.2)
+            }, completion: { Bool -> Void in
+
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 1.5, options: .layoutSubviews,
+            animations: {
+                tabView.transform = CGAffineTransform.identity.scaledBy(x: 1.0, y: 1.0)
+            }, completion: nil)
+        })
+    }
+
+//    private func animatePressedPostButton() {
+//        if postPressed == false {
+//            postPressed = true
+//            let postItemImageView = customTabBar.getIconView(for: .post)
+//            if let transform = postItemImageView.layer.presentation()?.affineTransform() {
+//                postItemImageView.layer.removeAllAnimations()
+//                postItemImageView.transform = transform
+//            }
+//
+//            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 1.5, options: [.curveEaseOut, .beginFromCurrentState], animations: {
+//                postItemImageView.transform = CGAffineTransform(rotationAngle: .pi / 4)
+//                postItemImageView.transform = postItemImageView.transform.scaledBy(x: 1.3, y: 1.3)
+//            }, completion: { _ in
+//
+//                UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
+//                    postItemImageView.transform = CGAffineTransform(rotationAngle: .pi / 4)
+//                    postItemImageView.transform = postItemImageView.transform.scaledBy(x: 1.0, y: 1.0)
+//                }, completion: nil)
+//            })
+//
+//        } else {
+//
+//           self.reset()
+//        }
+//    }
 }
